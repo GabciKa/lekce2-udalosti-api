318dd1483c431463c101a3c87701b0262e082548
gabiKa <gabi@klabal.cz>
2021-03-20 21:03:21 +0100

    lekce 2 komplet

--- main.js
+++ main.js
@@ -1,1 +1,69 @@
 // tady je místo pro náš program
+let nadpis;
+nadpis = document.querySelector("h1");
+
+// nadpis.classList.add("zluty");
+// nadpis.classList.remove("zluty");
+
+nadpis.classList.toggle("zluty");
+
+function secti(a, b) {
+    console.log(a + b); // vypíše při najetí na žlutý čtverec součet a + b
+}   
+
+function vypis(a,b) {
+    nadpis.innerHTML = a + b; //změní obsah nadpisu na výsledek a+b
+}
+
+let palindrom = document.querySelector("p");
+function tucny () {
+    palindrom.style.fontWeight = "bold";
+}
+
+function noBold() {
+    palindrom.style.fontWeight = "normal";
+}
+
+function cerveny () {
+    palindrom.classList.toggle("red-text");
+}
+
+function vetsiPismo() {
+    let odstavec = document.querySelector("p");
+    let vypocitany = window.getComputedStyle(odstavec);
+    let velikost = vypocitany.getPropertyValue("font-size");
+    console.log(velikost);
+    let novaVelikost = parseInt(velikost) + 1;
+    console.log(novaVelikost);
+    odstavec.style.fontSize = novaVelikost + "px";
+}
+
+function prehraj(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.play();
+}
+
+function zastav(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.pause();
+}
+
+function opakovat(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.load();
+}
+
+function ztisit(elementSelector){
+    let audio = document.getElementById("js-audio");
+    audio.volume = 0;
+}
+
+function hlasitostNormal(elementSelector){
+    let audio = document.getElementById("js-audio");
+    audio.volume = 0.5;
+}
+
+function naPlnyPecky(elementSelector){
+    let audio = document.getElementById("js-audio");
+    audio.volume = 1;
+}
